fac5aa07ae9ccad3b293c7bf6ae2f08f22ff6737
Ashok Kesari <31709130+ask5893@users.noreply.github.com>
2017-10-10 21:25:32 -0400

    Update Piece.java
    Added Javadoc comments

--- src/main/java/com/webcheckers/model/Piece.java
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -1,36 +1,67 @@
-package com.webcheckers.model;
+    package com.webcheckers.model;
 
 public class Piece {
     private String type;
     private String color;
 
+     /**
+     * This is the constructor for the "piece" class
+     * @param _type denotes if the piece is a "plain" piece or a "king" piece
+     * @param _color denotes if the piece is red or white
+     */
     public Piece(String _type, String _color) {
         this.type = _type;
         this.color = _color;
     }
 
-    public void movePiece(int _posX, int _posY){ }
-
-    public void capture(Piece _pieceA){ }
-
+     /**
+     * This function denotes the transformation of a "plain" piece to a "king" piece
+     */
     public void bceomeKing(){}
 
+     /**
+     * This function denotes if the move is a forward move or not
+     * @param _posY this is the Y co-ordinate of the piece
+     * @returns a Boolean "true" or "false"
+     */
     public boolean isMoveForward(int _posY){return true;}
 
+    /**
+     * This function denotes if the piece is captured or not
+     * @param _posX this is the X co-ordinate of the piece
+     * @param _posY this is the Y co-ordinate of the piece
+     * @return a Boolean "true" or "false"
+     */
     public  boolean isCapture(int _posX, int _posY ){return true;}
-
+    
+    /**
+     * This function is the accessor of color
+     * @return the color of the piece
+     */
     public String getColor() {
         return this.color;
     }
-
+    
+    /**
+     * This function is the accessor of type
+     * @return the type of piece (plain or king)
+     */
     public String getType() {
         return this.type;
     }
-
+    
+    /**
+     * This fuction is the mutator of color
+     * @param _color is the parameter that sets the color attribute
+     */
     public void setColor(String _color) {
         this.color = _color;
     }
-
+    
+    /**
+     * This function is the mutator of type
+     * @param _type is the parameter that sets the type attribute
+     */
     public void setType(String _type) {
         this.type = _type;
     }
