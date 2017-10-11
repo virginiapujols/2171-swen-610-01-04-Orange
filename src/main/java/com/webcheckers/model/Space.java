c8efc504c573a1c42b3c131e3bf61496ac5106a2
Ashok Kesari <31709130+ask5893@users.noreply.github.com>
2017-10-10 23:04:19 -0400

    Update Space.java
    Added Javadoc comments

--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -6,37 +6,70 @@ public class Space {
    private Piece piece;
    private int cellIdx;
 
-
+   /**
+     * This is the constructor for the Space class
+     * @param _color is the color of the space
+     * @param _piece is the piece in the space
+     * @param _cellIdx is the x-id of the space
+     */
    public Space(String _color, Piece _piece, int _cellIdx) {
        this.color = _color;
        this.piece = _piece;
        this.cellIdx = _cellIdx;
    }
-
+   
+   /**
+     * This function is the accessor of Color
+     * @return this is the color of the space
+     */
    public String getColor() {
        return this.color;
    }
-
+    
+    /**
+     * This function is the accessor of the piece
+     * @return this returns the piece in the space
+     */
     public Piece getPiece() {
        return this.piece;
     }
-
+   
+    /**
+     * This function is the accessor for the cell ID
+     * @return the cell ID
+     */
     public int getCellIdx() {
         return this.cellIdx;
     }
-
+   
+     /**
+     * This is the mutator for the color
+     * @param _color this sets the color of the space
+     */
     public void setColor(String _color) {
        this.color = _color;
     }
-
+   
+   /**
+     * This is the mutator of the piece
+     * @param _piece sets the value of piece in the space
+     */
     public void setPiece(Piece _piece) {
        this.piece = _piece;
     }
-
+   
+   /**
+     * This is the mutator of the cell ID
+     * @param _cellIdx this sets the cell ID
+     */
     public void setCellIdx(int _cellIdx) {
        this.cellIdx = _cellIdx;
     }
-
+   
+   /**
+     * This function returns if it is valid or nor
+     * @return a Boolean "true" or "false"
+     */
     public boolean isValid() {
        return this.piece == null && this.color.equals("black");
     }
