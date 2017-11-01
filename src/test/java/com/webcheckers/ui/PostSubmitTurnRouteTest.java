cd011e98b64919e94e3155089cd24ad32a7a591b
Niharika <niharika13dalal@gmail.com>
2017-10-31 20:36:00 -0400

    Added test case for the Handle functionality of PostSubmitTurnRoute. Test is positive.

--- src/test/java/com/webcheckers/ui/PostSubmitTurnRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostSubmitTurnRouteTest.java
@@ -22,4 +22,23 @@ public class PostSubmitTurnRouteTest {
             assertEquals("gameCenter must not be null", e.getMessage());
         }
     }
+
+    @Test
+    public void testHandle() throws Exception {
+        Session session = mock(Session.class);
+        Request request = mock(Request.class);
+        Response response = mock(Response.class);
+        GameCenter gameCenter = mock(GameCenter.class);
+        Game test = mock(Game.class);
+        PostLoginRoute postLoginRoute = new PostLoginRoute(gameCenter);
+        String _player1 = "niharika";
+        String _player2 = "virginia";
+
+        when(request.session()).thenReturn(session);
+        when(session.attribute(PostLoginRoute.USERNAME_PARAM)).thenReturn(_player1);
+        when(gameCenter.getGame(request.session().attribute(PostLoginRoute.USERNAME_PARAM))).thenReturn(test);
+        when(test.changeTurn()).thenReturn(1);
+
+        assertNotNull(postLoginRoute.handle(request, response));
+    }
 }
