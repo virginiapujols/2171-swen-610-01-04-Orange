6c28df179ca3f662465b8e2ca847d7c4620d531f
Ashok Kesari <31709130+ask5893@users.noreply.github.com>
2017-11-01 18:44:36 -0400

    GetLogoutRoute is tested

---
+++ src/test/java/com/webcheckers/ui/GetLogoutRouteTest.java
@@ -0,0 +1,56 @@
+package com.webcheckers.ui;
+
+import static org.junit.Assert.*;
+
+import com.webcheckers.appl.GameCenter;
+import org.junit.Test;
+import org.mockito.Mock;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Handler;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockingDetails;
+import static org.mockito.Mockito.when;
+
+public class GetLogoutRouteTest {
+    @Test
+    public void testGetLogoutRoute() throws Exception {
+
+        try {
+
+            GetLogoutRoute test = new GetLogoutRoute(null);
+
+        } catch (NullPointerException e) {
+
+            assertEquals("gameCenter must not be null", e.getMessage());
+
+        }
+
+    }
+
+    @Test
+    public void handle(){
+        GameCenter gameCenter = mock(GameCenter.class);
+        GetLogoutRoute test = new GetLogoutRoute(gameCenter);
+        Request request = mock(Request.class);
+        Session session = mock(Session.class);
+
+        when(request.session()).thenReturn(session);
+        when(session.attribute("username")).thenReturn("ashok");
+        gameCenter.addPlayer(session, "ashok");
+        Response response = mock(Response.class);
+        ModelAndView view = test.handle(request, response);
+       Map<String, Object> vm =(Map<String, Object>) view.getModel();
+        assertEquals("Login!",vm.get("title"));
+        assertEquals(GetHomeRoute.VIEW_NAME, view.getViewName());
+        assertEquals(null,vm.get("username"));
+        assertNull(vm.get("username"));
+    }
+}
