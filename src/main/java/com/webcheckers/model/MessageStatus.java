663239ee44ece4fcbdbf5678bea3a4314bb9f89c
virginia.pujols <virginia.pujols@edalsolutions.com>
2017-11-14 12:24:30 -0500

    Enums created to apply state behaviour

--- src/main/java/com/webcheckers/model/MessageStatus.java
+++ src/main/java/com/webcheckers/model/MessageStatus.java
@@ -1,4 +1,15 @@
 package com.webcheckers.model;
 
 public enum MessageStatus {
+    INFO("info"),
+    ERROR("error");
+
+    private String description;
+    MessageStatus(String name) {
+        this.description = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
--- src/main/java/com/webcheckers/model/PieceStatus.java
+++ src/main/java/com/webcheckers/model/PieceStatus.java
@@ -1,4 +1,15 @@
 package com.webcheckers.model;
 
 public enum PieceStatus {
+    SINGLE("SINGLE"),
+    KING("KING");
+
+    private String description;
+    PieceStatus(String name) {
+        this.description = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
