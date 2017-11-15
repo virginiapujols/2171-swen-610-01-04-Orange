ed3f981ee231a14ec588d1c9811be72f6f43be9d
Niharika <niharika13dalal@gmail.com>
2017-11-15 10:37:01 -0500

    Modified test case for handle() method of PostResignGameRoute class. Tests are positive.

--- src/test/java/com/webcheckers/ui/PostResignGameRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostResignGameRouteTest.java
@@ -2,6 +2,7 @@ package com.webcheckers.ui;
 
 import com.webcheckers.appl.GameCenter;
 import com.webcheckers.model.Game;
+import org.junit.Before;
 import org.junit.Test;
 import spark.ModelAndView;
 import spark.Request;
@@ -14,22 +15,33 @@ import static org.mockito.Mockito.when;
 
 public class PostResignGameRouteTest {
 
+    // Atributes
+    private GameCenter gameCenter = mock(GameCenter.class);
+    private String _username = "Niharika";
+    private Game game;
+    private Request request;
+    private Response response;
+    private Session session;
+
+    /**
+     * The component under test(CuT)
+     */
+    private PostResignGameRoute CuT = new PostResignGameRoute(gameCenter);
+
+    @Before
+    public void setUp(){
+        game = mock(Game.class);
+        request = mock(Request.class);
+        response = mock(Response.class);
+        session = mock(Session.class);
+    }
+
     @Test (expected = spark.HaltException.class)
     public void handle() throws Exception {
-        GameCenter gameCenter = mock(GameCenter.class);
-        Game game = mock(Game.class);
-        PostResignGameRoute test = new PostResignGameRoute(gameCenter);
-        Request request = mock(Request.class);
-        Response response = mock(Response.class);
-        Session session = mock(Session.class);
-        HashMap<String, Object> vm = new HashMap<>();
-        String _username = "Niharika";
-
         when(request.session()).thenReturn(session);
         when(session.attribute(PostLoginRoute.USERNAME_PARAM)).thenReturn(_username);
         when(gameCenter.getGame(_username)).thenReturn(game);
-
-        assertNull(test.handle(request, response));
+        assertNull(CuT.handle(request, response));
     }
 
 }
