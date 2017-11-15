feeb2129621ee67242941158458d1824bdcd4e78
Niharika <niharika13dalal@gmail.com>
2017-11-15 10:45:14 -0500

    Modified test case for handle() method of PostBackupMoveRoute class. Tests are positive.

--- src/test/java/com/webcheckers/ui/PostBackupMoveRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostBackupMoveRouteTest.java
@@ -3,32 +3,50 @@ package com.webcheckers.ui;
 import com.webcheckers.appl.GameCenter;
 import com.webcheckers.model.Game;
 import com.webcheckers.model.Message;
+import org.junit.Before;
 import org.junit.Test;
 import spark.Request;
 import spark.Response;
 import spark.Session;
+
+import java.util.jar.Attributes;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class PostBackupMoveRouteTest {
+    private GameCenter gameCenter = mock(GameCenter.class);
+    private String _username = "Niharika";
+    private Game game;
+    private Request request;
+    private Response response;
+    private Session session;
+    private Message msg;
+
+    /**
+     * The component under test(CuT)
+     */
+    private PostBackupMoveRoute CuT = new PostBackupMoveRoute(gameCenter);
+
+    @Before
+    public void setUp(){
+        game = mock(Game.class);
+        request = mock(Request.class);
+        response = mock(Response.class);
+        session = mock(Session.class);
+        msg = mock(Message.class);
+    }
+
+
     @Test
     public void testHandle() throws Exception {
-        Request request = mock(Request.class);
-        Response response = mock(Response.class);
-        Session session = mock(Session.class);
-        GameCenter gameCenter = mock(GameCenter.class);
-        String _username = "niharika";
-        Game game = mock(Game.class);
-        PostBackupMoveRoute test = new PostBackupMoveRoute(gameCenter);
         when(request.session()).thenReturn(session);
         when(session.attribute(PostLoginRoute.USERNAME_PARAM)).thenReturn(_username);
         when(gameCenter.getGame(_username)).thenReturn(game);
-        Message msg = mock(Message.class);
         when(game.backupMove()).thenReturn(msg);
 
-        assertEquals(msg, test.handle(request, response));
+        assertEquals(msg, CuT.handle(request, response));
     }
 
 }
